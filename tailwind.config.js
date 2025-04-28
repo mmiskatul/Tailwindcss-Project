fbad2ca3b55bbd893dcd4916e562a6afae095e7f
mmiskatul <masabimiskat@gmail.com>
2025-04-28 10:43:18 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Roboto', 'sans-serif'], // Add 'Roboto' as the sans-serif font
+          'poppins': ['Poppins', 'sans-serif'], // Add 'Poppins' as the sans-serif font
+        'inter': ['Inter', 'sans-serif'], // Add 'Inter' as the sans-serif font 
+        'montserrat': ['Montserrat', 'sans-serif'], // Add 'Montserrat' as the sans-serif font
+        'lato': ['Lato', 'sans-serif'], // Add 'Lato' as the sans-serif font
+        'open-sans': ['Open Sans', 'sans-serif'], // Add 'Open Sans' as the sans-serif font
+        'roboto-slab': ['Roboto Slab', 'serif'], // Add 'Roboto Slab' as the serif font
+        'merriweather': ['Merriweather', 'serif'], // Add 'Merriweather' as the serif font
+        'playfair-display': ['Playfair Display', 'serif'], // Add 'Playfair Display' as the serif font
+        'source-sans-pro': ['Source Sans Pro', 'sans-serif'], // Add 'Source Sans Pro' as the sans-serif font
+        
+        },
+      },
+    },
+    plugins: [],
+  };
+  
