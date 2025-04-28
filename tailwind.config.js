3da6c2f3d3585f7ae8096d4290d7cca6654d0162
mmiskatul <masabimiskat@gmail.com>
2025-04-28 10:59:06 +0600

    add nav bar icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,9 @@ module.exports = {
     theme: {
       extend: {
         fontFamily: {
-          sans: ['Roboto', 'sans-serif'], // Add 'Roboto' as the sans-serif font
-          'poppins': ['Poppins', 'sans-serif'], // Add 'Poppins' as the sans-serif font
-        'inter': ['Inter', 'sans-serif'], // Add 'Inter' as the sans-serif font 
-        'montserrat': ['Montserrat', 'sans-serif'], // Add 'Montserrat' as the sans-serif font
-        'lato': ['Lato', 'sans-serif'], // Add 'Lato' as the sans-serif font
-        'open-sans': ['Open Sans', 'sans-serif'], // Add 'Open Sans' as the sans-serif font
-        'roboto-slab': ['Roboto Slab', 'serif'], // Add 'Roboto Slab' as the serif font
-        'merriweather': ['Merriweather', 'serif'], // Add 'Merriweather' as the serif font
-        'playfair-display': ['Playfair Display', 'serif'], // Add 'Playfair Display' as the serif font
-        'source-sans-pro': ['Source Sans Pro', 'sans-serif'], // Add 'Source Sans Pro' as the sans-serif font
-        
+          'display':['poppins','sans-serif'],
+          'body':['Inter','sans-serif'],
+
         },
       },
     },
